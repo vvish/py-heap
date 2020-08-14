08970f0e44a1ccafc73a02a209e8ea196da1613e
vvish <v.v.swe.os@gmail.com>
2020-08-14 17:57:10 +0200

    Reduced redundancy in the test code (#12)

--- tests/indexed_heap_test.py
+++ tests/indexed_heap_test.py
@@ -1,5 +1,3 @@
-import copy
-
 import pytest
 
 from indexed_heap import IndexedHeap, sort
@@ -17,7 +15,7 @@ def is_heap(items_to_check, start_position, compare_fn):
     items_to_check : array_like
         array of items to check
     start_position : integer
-        offset of items inside array to check 
+        offset of items inside array to check
     compare_fn : callable
         comparator function defining order to check agains
 
@@ -76,6 +74,55 @@ def test_utils_single_item_is_min_heap():
     )
 
 
+def are_sorted_heaps_equal(first_heap, second_heap):
+    """
+    Determine if the heaps produce identical sequences when heap-sorted.
+
+    Applying sorting to two heaps and comparing output is the way to
+    determine if two heaps are equivalent.
+
+    As function continiously extracts elements from the heaps, the heaps
+    will be emptied after function call
+
+    Parameters
+    ----------
+    first_heap : IndexedHeap
+        first heap to compare
+    second_heap : IndexedHeap
+        second heap to compare
+
+    Returns
+    -------
+    bool
+        True if two heaps produce identical sorted sequences
+    """
+
+    return [
+        first_heap.pop_first() for _ in range(len(first_heap))
+    ] == [
+        second_heap.pop_first() for _ in range(len(second_heap))
+    ]
+
+
+def test_utils_are_sorted_heaps_equal():
+    heap1 = IndexedHeap({1: 'A', 2: 'B'})
+    heap2 = IndexedHeap({1: 'C', 2: 'D'})
+
+    assert not are_sorted_heaps_equal(heap1, heap2)
+    assert not list(heap1)
+    assert not list(heap2)
+
+    assert are_sorted_heaps_equal(
+        IndexedHeap({1: 'A', 2: 'B'}),
+        IndexedHeap({1: 'A', 2: 'B'}),
+    )
+
+    assert are_sorted_heaps_equal(
+        IndexedHeap(),
+        IndexedHeap(),
+    )
+
+
 def test_empty_heap_creation():
     heap = IndexedHeap()
     assert len(heap) == 0, 'The length should be equal to 0'
@@ -84,168 +131,51 @@ def test_empty_heap_creation():
     assert heap.pop_first() == (None, None), 'Pop oeration should return None'
 
 
-class DefaultHeapFixture(object):
-    test_values = {1: 4, 2: 2, 3: 3, 4: 1, 5: 5}
-
-    def validate(self, heap):
-        self.validate_default_length(heap)
-        self.validate_items_presense(heap)
-        self.validate_default_order(heap)
-
-        heap_copy = copy.deepcopy(heap)
-        self.validate_default_extraction(heap_copy)
-
-    def validate_default_length(self, heap):
-        assert len(heap) == 5, 'The length should be equal to 5'
-        assert len(heap) == len(heap.get_items(
-        )), 'The length should be exact as the number of the stored elements'
-
-    def validate_items_presense(self, heap):
-        assert all(
-            key in heap for key in self.test_values.keys()
-        ), 'In operator should provide valid results'
-        assert (
-            heap[1], heap[2], heap[3], heap[4], heap[5],
-        ) == (
-            4, 2, 3, 1, 5,
-        ), 'Items should be accessible via keys'
-
-    def validate_default_order(self, heap):
-        assert is_heap(
-            heap.get_items(),
-            start_position=0,
-            compare_fn=lambda first, second: first[1] <= second[1],
-        ), 'Min heap should be created by default'
-
-    def validate_reversed_order(self, heap):
-        assert is_heap(
-            heap.get_items(),
-            start_position=0,
-            compare_fn=lambda first, second: first[1] >= second[1],
-        ), 'Max heap should be created'
-
-    def validate_default_extraction(self, heap):
-        assert (
-            heap.pop_first(),
-            heap.pop_first(),
-            heap.pop_first(),
-            heap.pop_first(),
-            heap.pop_first(),
-        ) == (
-            (4, 1), (2, 2), (3, 3), (1, 4), (5, 5),
-        ), 'The items should be poped in sorted order'
-
-
-@pytest.fixture
-def default_heap_fixture():
-    return DefaultHeapFixture()
-
-
-def test_heap_items_added_via_update(default_heap_fixture):
+def test_heap_len():
     heap = IndexedHeap()
-    heap.update(default_heap_fixture.test_values)
+    assert len(heap) == 0, 'Length of empty heap should be equal to 0'
+    assert len(heap) == len(
+        list(heap),
+    ), 'The length should be exact the number of the stored elements'
 
-    default_heap_fixture.validate(heap)
-
-
-def test_heap_items_added_via_subscript(default_heap_fixture):
-    heap = IndexedHeap()
-
-    for k, v in default_heap_fixture.test_values.items():
-        heap[k] = v
-
-    default_heap_fixture.validate(heap)
+    heap = IndexedHeap({1: 'A', 2: 'B'})
+    assert len(heap) == 2, 'Heap length should be equal to 2'
+    assert len(heap) == len(
+        list(heap),
+    ), 'The length should be exact the number of the stored elements'
 
 
-def test_heap_items_specified_in_init(default_heap_fixture):
-    heap = IndexedHeap(default_heap_fixture.test_values)
-    default_heap_fixture.validate(heap)
-
-
-def test_heap_reversed_and_items_specified_in_init(default_heap_fixture):
-    heap = IndexedHeap(default_heap_fixture.test_values, reverse=True)
-
-    default_heap_fixture.validate_default_length(heap)
-    default_heap_fixture.validate_items_presense(heap)
-
-    default_heap_fixture.validate_reversed_order(heap)
-
-    assert (
-        heap.pop_first(),
-        heap.pop_first(),
-        heap.pop_first(),
-        heap.pop_first(),
-        heap.pop_first(),
-    ) == (
-        (5, 5), (1, 4), (3, 3), (2, 2), (4, 1),
-    ), 'The items should be poped in sorted order'
-
-
-class UpdatedHeapFixture(DefaultHeapFixture):
-    updated_values = {4: 10, 3: 11}
-
-    def validate_updated(self, heap):
-        self.validate_default_length(heap)
-        self.validate_default_order(heap)
-
-        assert all(
-            key in heap for key in self.test_values.keys()
-        ), 'In operator should provide valid results'
-
-        assert (
-            heap[1], heap[2], heap[3], heap[4], heap[5],
-        ) == (
-            4, 2, 11, 10, 5,
-        ), 'Items should be accessible via keys'
-
-        assert (
-            heap.pop_first(),
-            heap.pop_first(),
-            heap.pop_first(),
-            heap.pop_first(),
-            heap.pop_first(),
-        ) == (
-            (2, 2), (1, 4), (5, 5), (4, 10), (3, 11),
-        ), 'The items should be poped in sorted order'
-
-
-@pytest.fixture
-def update_test_fixture():
-    return UpdatedHeapFixture()
-
-
-def test_heap_item_is_updated_via_subscript(update_test_fixture):
-    heap = IndexedHeap(update_test_fixture.test_values)
-
-    for k, v in update_test_fixture.updated_values.items():
-        heap[k] = v
-
-    update_test_fixture.validate_updated(heap)
-
-
-def test_heap_is_updated_via_update_heap_reordered(update_test_fixture):
-    heap = IndexedHeap(update_test_fixture.test_values)
-    heap.update(update_test_fixture.updated_values)
-
-    update_test_fixture.validate_updated(heap)
-
-
-def test_heap_is_updated_via_subscript_key_decreased(default_heap_fixture):
-    heap = IndexedHeap({1: 4, 2: 2, 3: 3, 4: 1, 5: 5})
-    heap[5] = 0
-
-    default_heap_fixture.validate_default_length(heap)
-    default_heap_fixture.validate_default_order(heap)
+def test_heap_check_key():
+    test_values = {1: 'A', 2: 'B', 3: 'C'}
+    heap = IndexedHeap(test_values)
 
     assert all(
-        key in heap for key in default_heap_fixture.test_values.keys()
+        key in heap for key in test_values
     ), 'In operator should provide valid results'
+
+    assert 6 not in heap, 'Value with key 6 should not be found'
+
+
+def test_heap_get_item_via_index():
+    test_values = {1: 4, 2: 2, 3: 3, 4: 1, 5: 5}
+
+    heap = IndexedHeap(test_values)
 
     assert (
         heap[1], heap[2], heap[3], heap[4], heap[5],
     ) == (
-        4, 2, 3, 1, 0,
+        4, 2, 3, 1, 5,
     ), 'Items should be accessible via keys'
+
+def test_heap_default_order():
+    test_values = {1: 4, 2: 2, 3: 3, 4: 1, 5: 5}
+
+    heap = IndexedHeap(test_values)
+    assert is_heap(
+        heap.get_items(),
+        start_position=0,
+        compare_fn=lambda first, second: first[1] <= second[1],
+    ), 'Min heap should be created by default'
 
     assert (
         heap.pop_first(),
@@ -254,42 +184,91 @@ def test_heap_is_updated_via_subscript_key_decreased(default_heap_fixture):
         heap.pop_first(),
         heap.pop_first(),
     ) == (
-        (5, 0), (4, 1), (2, 2), (3, 3), (1, 4),
+        (4, 1), (2, 2), (3, 3), (1, 4), (5, 5),
+    ), 'The items should be poped in sorted order'
+
+def test_heap_reversed_order():
+    test_values = {1: 4, 2: 2, 4: 1, 5: 5}
+    heap = IndexedHeap(test_values, reverse=True)
+
+    assert is_heap(
+        heap.get_items(),
+        start_position=0,
+        compare_fn=lambda first, second: first[1] >= second[1],
+    ), 'Max heap should be created'
+
+    assert (
+        heap.pop_first(),
+        heap.pop_first(),
+        heap.pop_first(),
+        heap.pop_first(),
+    ) == (
+        (5, 5), (1, 4), (2, 2), (4, 1)  
     ), 'The items should be poped in sorted order'
 
 
-def test_heap_item_delete(default_heap_fixture):
-    heap = IndexedHeap(default_heap_fixture.test_values)
+def test_heap_update():
+    test_values = {1: 'A', 2: 'B', 3: 'C'}
+
+    heap = IndexedHeap()
+    heap.update(test_values)
+    assert are_sorted_heaps_equal(
+        heap,
+        IndexedHeap(test_values),
+    ), 'Two heaps should be equal'
+
+    heap.update(test_values)
+    heap.update({3: 'X', 4: 'Z'})
+    assert are_sorted_heaps_equal(
+        heap,
+        IndexedHeap({1: 'A', 2: 'B', 3: 'X', 4: 'Z'}),
+    ), 'Two heaps should be equal'
+
+
+def test_set_item_via_index():
+    heap = IndexedHeap()
+    heap[1] = 'A'
+    heap[2] = 'B'
+    heap[3] = 'C'
+    assert are_sorted_heaps_equal(
+        heap,
+        IndexedHeap({1: 'A', 2: 'B', 3: 'C'}),
+    ), 'Two heaps should be equal'
+
+    heap[1] = 'A'
+    heap[2] = 'B'
+    heap[3] = 'C'
+    heap[3] = 'X'
+    assert are_sorted_heaps_equal(
+        heap,
+        IndexedHeap({1: 'A', 2: 'B', 3: 'X'}),
+    ), 'Two heaps should be equal'
+
+def test_heap_update_item_key_decreased():
+    heap = IndexedHeap({1: 4, 2: 2, 3: 3, 4: 1, 5: 5})
+    heap[5] = 0
+
+    assert is_heap(
+        heap.get_items(),
+        start_position=0,
+        compare_fn=lambda first, second: first[1] <= second[1],
+    ), 'Min heap should be created by default'
+
+    list(
+        sort(heap),
+    ) == [
+        (5, 0), (4, 1), (2, 2), (3, 3), (1, 4),
+    ], 'The items should be poped in sorted order'
+
+def test_heap_delete_item():
+    heap = IndexedHeap({1: 4, 2: 2, 3: 3, 4: 1, 5: 5})
 
     del heap[3]
-
-    assert len(heap) == 4, 'The length after delete should be equal to 5'
-    assert len(heap) == len(heap.get_items(
-    )), 'The length should be exact as the number of the stored elements'
-
-    default_heap_fixture.validate_default_order(heap)
-
-    assert all(
-        key in heap for key in (1, 2, 4, 5)
-    ), 'In operator should provide valid results'
     assert 3 not in heap, 'Key `3` should not be in the heap'
-
-    assert (
-        heap[1], heap[2], heap[4], heap[5],
-    ) == (
-        4, 2, 1, 5,
-    ), 'Items should be accessible via keys'
-
-    assert (
-        heap.pop_first(),
-        heap.pop_first(),
-        heap.pop_first(),
-        heap.pop_first(),
-    ) == (
-        (4, 1), (2, 2), (1, 4), (5, 5),
-    ), 'The items should be poped in sorted order'
-
-    assert len(heap) == 0, 'The length after poping items should be equal to 5'
+    assert are_sorted_heaps_equal(
+        heap,
+        IndexedHeap({1: 4, 2: 2, 4: 1, 5: 5}),
+    ), 'Two heaps should be equal'
 
 
 def test_heap_created_with_custom_key_second_value_used_as_key():
@@ -311,18 +290,11 @@ def test_heap_created_with_custom_key_second_value_used_as_key():
         key=lambda i: i.key,
     )
 
-    assert len(heap) == 5, 'The length should be equal to 5'
-    assert len(heap) == len(heap.get_items(
-    )), 'The length should be exact as the number of the stored elements'
     assert is_heap(
         heap.get_items(),
         start_position=0,
         compare_fn=lambda first, second: first[1].key <= second[1].key
     ), 'Min heap should be created by default'
-
-    assert all(
-        key in heap for key in (1, 2, 3, 4, 5)
-    ), 'In operator should provide valid results'
 
     assert (
         heap[1], heap[2], heap[3], heap[4], heap[5],
@@ -330,28 +302,24 @@ def test_heap_created_with_custom_key_second_value_used_as_key():
         Value(1), Value(2), Value(3), Value(4), Value(5),
     ), 'Items should be accessible via keys'
 
-    assert (
-        heap.pop_first(),
-        heap.pop_first(),
-        heap.pop_first(),
-        heap.pop_first(),
-        heap.pop_first(),
-    ) == (
+    list(
+        sort(heap),
+    ) == [
         (1, Value(1)),
         (2, Value(2)),
         (3, Value(3)),
         (4, Value(4)),
         (5, Value(5)),
-    ), 'The items should be poped in sorted order'
+    ], 'The items should be poped in sorted order'
 
 
-def test_sort():
+def test_heap_sort():
     heap = IndexedHeap({1: 4, 2: 2, 3: 3, 4: 1, 5: 5})
 
     assert list(sort(heap)) == [(4, 1), (2, 2), (3, 3), (1, 4), (5, 5)]
 
 
-def test_iterator():
+def test_heap_iterator():
     heap = IndexedHeap({1: 4, 2: 2, 3: 3, 4: 1, 5: 5})
 
     iterated_items = [it for it in heap]
